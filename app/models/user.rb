69960d47d07d5aebfb8f5ca9e07e331cb486bdd3
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-24 00:21:41 +0900

    [Fix]綴り修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { minimum: 2, maxium: 20}
-  validates :introduction, presence: true, length: { maxium: 50}
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :introduction, presence: true, length: { maximum: 50}
 
   has_many :books, dependent: :destroy
   attachment :progile_image

e448451694d2181819726882ce645d5403a64adf
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-25 01:52:20 +0900

    [Fix]コード修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,17 @@ class BooksController < ApplicationController
     @book = Book.new
     @book = Book.find(params[:id])
   end
+  
+  def update
+  @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:complete]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
 
   def destroy
     book = Book.find(params[:id])
@@ -36,18 +47,6 @@ class BooksController < ApplicationController
       redirect_to books_path
     end
   end
-
-    def update
-    @book = Book.find(params[:id])
-      if @book.update(book_params)
-        flash[:complete]="Book was successfully updated."
-        redirect_to book_path(@book.id)
-      else
-        @books = Book.all
-        render :edit
-      end
-    end
-
   private
     def book_params
       params.require(:book).permit(:title, :body)

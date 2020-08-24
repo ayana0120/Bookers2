de7b5acc5a426435fe99fe0e3f3b656d76d4e4d3
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-24 12:57:25 +0900

    [Add]optional追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ApplicationRecord
-  belongs_to :user
-  attachment :profilr_image
+  belongs_to :user, optional: true
+  attachment :profile_image
 
   validates :title, presence: true
-  validates :opinion, presence: true
-  validates :body, presence: true,length: { maximum: 200}
+  validates :body, presence: true, length: { maximum: 200}
 end
